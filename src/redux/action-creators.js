62f926d395967df84e17cf419f3b00dc852f34bc
DenisKenzo <31656851+DenisKenzo@users.noreply.github.com>
2021-02-04 14:42:05 +0200

    typescript edits in redux

--- src/redux/action-creators.js
+++
@@ -1,9 +0,0 @@
-import {
-    ADD_NEW_COMMENT,
-    ADD_NEW_POST,
-    DELETE_POST,
-} from "./constants";
-
-export const addNewPostAC = (text) => ({type: ADD_NEW_POST, payload: text})
-export const addNewCommentAC = (id, text) => ({type: ADD_NEW_COMMENT, id: id, payload: text})
-export const deletePostAC = id => ({type: DELETE_POST, payload: id})
